2d218a47ebb6ac62247eb517a1844c6a9385a6e7
CristofferG <134312678+CristofferG@users.noreply.github.com>
2024-06-12 16:42:24 -0400

    Enemigo Listo
    Hice "Arbol maldito" con su animacion y interaccion con el player.

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,13 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    public bool FacingLeft { get {return facingLeft;} }
+    public bool FacingLeft { get { return facingLeft; } }
     public static PlayerController Instance;
-    [SerializeField] private float moveSpeed = 4f;
+
+    [SerializeField] private float moveSpeed = 1f;
     [SerializeField] private float dashSpeed = 4f;
     [SerializeField] private TrailRenderer myTrailRenderer;
 
@@ -15,21 +15,23 @@ public class PlayerController : MonoBehaviour
     private Vector2 movement;
     private Rigidbody2D rb;
     private Animator myAnimator;
-    private SpriteRenderer mySpriteRenderer;
+    private SpriteRenderer mySpriteRender;
     private float startingMoveSpeed;
 
     private bool facingLeft = false;
     private bool isDashing = false;
+
     private void Awake() {
         Instance = this;
         playerControls = new PlayerControls();
         rb = GetComponent<Rigidbody2D>();
         myAnimator = GetComponent<Animator>();
-        mySpriteRenderer = GetComponent<SpriteRenderer>();
+        mySpriteRender = GetComponent<SpriteRenderer>();
     }
 
     private void Start() {
         playerControls.Combat.Dash.performed += _ => Dash();
+
         startingMoveSpeed = moveSpeed;
     }
 
@@ -46,7 +48,6 @@ public class PlayerController : MonoBehaviour
         Move();
     }
 
-    // recibir el input del jugador
     private void PlayerInput() {
         movement = playerControls.Movement.Move.ReadValue<Vector2>();
 
@@ -54,21 +55,19 @@ public class PlayerController : MonoBehaviour
         myAnimator.SetFloat("moveY", movement.y);
     }
 
-    // mover al jugador
     private void Move() {
         rb.MovePosition(rb.position + movement * (moveSpeed * Time.fixedDeltaTime));
     }
 
-    // ajustar la dirección del jugador
     private void AdjustPlayerFacingDirection() {
         Vector3 mousePos = Input.mousePosition;
         Vector3 playerScreenPoint = Camera.main.WorldToScreenPoint(transform.position);
 
         if (mousePos.x < playerScreenPoint.x) {
-            mySpriteRenderer.flipX = true;
+            mySpriteRender.flipX = true;
             facingLeft = true;
         } else {
-            mySpriteRenderer.flipX = false;
+            mySpriteRender.flipX = false;
             facingLeft = false;
         }
     }
